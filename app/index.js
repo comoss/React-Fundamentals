3634992fd4b9a7b8c6816761f7641287e44a808c
Cody Moss <codytyler.moss@gmail.com>
2016-05-11 22:15:46 -0600

    code for video 3

--- app/index.js
+++ app/index.js
@@ -1,15 +1,61 @@
+var USER_DATA = {
+  name: 'Cody Moss',
+  username: 'comoss',
+  image: 'https://avatars2.githubusercontent.com/u/7980830?v=3&s=460',
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+// Focused
+// Independent
+// Reusable
+// Small
+// Testable
+
+var ProfilePic = React.createClass({
   render: function () {
     return (
-       <div> Hello Reactjs Program! </div>
+    <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
     )
   }
 });
 
+var ProfileLink = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <a href={'https://github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function () {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+});
+
+
+
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
