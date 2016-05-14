e8070d5f10a51bfb68fc6bde1b3df34546e37f6f
Cody Moss <codytyler.moss@gmail.com>
2016-05-14 16:23:18 -0600

    added some routes and logic

--- app/components/main.js
+++ app/components/main.js
@@ -1,11 +1,13 @@
 var React = require('react');
 
+
 var Main = React.createClass({
   render: function () {
     return (
       <div className='main-container'>
-      {this.props.children}
+        {this.props.children}
       </div>
+
     )
   }
 })
